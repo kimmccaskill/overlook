f5d3b54ec767ca8a62f3658a188d3efabf9860f6
Kim McCaskill <kimmccaskill92@gmail.com>
2020-01-09 19:01:03 -0700

    Add Promise all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import $ from 'jquery';
 import domUpdates from './domUpdates.js'
 import './css/base.scss';
+
+let userData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
+  .then(response => response.json())
+  .then(data => data.users);
+
+let roomData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
+  .then(response => response.json())
+  .then(data => data.rooms);
+
+let bookingData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
+  .then(response => response.json())
+  .then(data => data.bookings);
+
+Promise.all([userData, roomData, bookingData])
+  .then(data => {
+    userData = data[0];
+    roomData = data[1];
+    bookingData = data[2];
+  })
+  .then(response => doThing());
+
+const doThing = () => {
+  console.log(userData)
+}
