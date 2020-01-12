d75ca233c8a8f2369c9cae80c5306e05dab4c460
Kim McCaskill <kimmccaskill92@gmail.com>
2020-01-11 17:36:02 -0700

    Add getroomsbooked

--- src/Bookings.js
+++ src/Bookings.js
@@ -4,7 +4,16 @@ class Bookings {
     this.bookings = bookings;
   }
 
-  
-
+  getRoomsBooked(date) {
+    return this.rooms.reduce((acc, room) => {
+      this.bookings.forEach(booking =>{
+        if (room.number === booking.roomNumber && booking.date === date) {
+          acc.push(booking)
+        }
+      })
+      return acc
+    }, [])
+  }
 }
+
 export default Bookings;
