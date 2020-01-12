28b52ed2301df9abe9df0e03a043f77bb68ff9ed
Kim McCaskill <kimmccaskill92@gmail.com>
2020-01-11 23:21:46 -0700

    Add bookings propertty

--- src/User.js
+++ src/User.js
@@ -1,10 +1,12 @@
 import Bookings from "./Bookings"
 
 class User {
-  constructor(id, name) {
+  constructor(id, name, bookings) {
     this.id = id;
     this.name = name;
+    this.bookings = bookings;
   }
+
 
 }
 export default User;
