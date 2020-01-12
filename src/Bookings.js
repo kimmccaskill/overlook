42a3b5e4202ae846e057233e38b8731a183cccdc
Kim McCaskill <kimmccaskill92@gmail.com>
2020-01-11 18:06:39 -0700

    Add roomsavail method

--- src/Bookings.js
+++ src/Bookings.js
@@ -4,11 +4,15 @@ class Bookings {
     this.bookings = bookings;
   }
 
+  getRoomsAvailable(date) {
+    return this.rooms.length - this.getRoomsBooked(date).length
+  }
+
   getRoomsBooked(date) {
     return this.rooms.reduce((acc, room) => {
       this.bookings.forEach(booking =>{
         if (room.number === booking.roomNumber && booking.date === date) {
-          acc.push(booking)
+          acc.push(booking.roomNumber)
         }
       })
       return acc
