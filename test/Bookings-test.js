52ddcbb0c6c7ed8ba9a69b9732f543e621b106fb
Kim McCaskill <kimmccaskill92@gmail.com>
2020-01-13 14:50:46 -0700

    Add test

--- test/Bookings-test.js
+++ test/Bookings-test.js
@@ -1,10 +1,29 @@
 import chai from 'chai';
 const expect = chai.expect;
-
 import Bookings from "../src/Bookings"
+import rooms from "./rooms-data"
+import bookings from "./bookings-data"
 
-describe('See if the tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
+let bookingClass;
+
+describe('Class Bookings', function() {
+  beforeEach(() => {
+    bookingClass = new Bookings(rooms, bookings)
+  });
+
+  it('should hold rooms data', function() {
+    expect(bookingClass.rooms).to.deep.equal(rooms);
+  });
+
+  it('should hold bookings data', function() {
+    expect(bookingClass.bookings).to.deep.equal(bookings);
+  });
+
+  it('should get how many rooms are available', function() {
+    expect(bookingClass.getRoomsAvailable("2020/02/04")).to.deep.equal(24);
+  });
+
+  it('should get bookings for specific date', function() {
+    expect(bookingClass.getRoomsBooked("2020/02/04")).to.deep.equal([15]);
   });
 });
