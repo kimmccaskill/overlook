851800430ef994d2eb066074626a8690e414a479
Kim McCaskill <kimmccaskill92@gmail.com>
2020-01-13 20:39:51 -0700

    Add auto complete feature

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Guest from './Guest';
 import Manager from './Manager'
 import Bookings from './Bookings';
 
-let currentUser, bookings, userBookings, selectedDate;
+let currentUser, bookings, userBookings, selectedDate, guestNames;
 let todaysDate = new Date();
 let dd = String(todaysDate.getDate()).padStart(2, '0');
 let mm = String(todaysDate.getMonth() + 1).padStart(2, '0');
@@ -95,6 +95,11 @@ const loadManagerDashboard = () => {
   loadRoomsAvailable();
   loadRevenue();
   loadRoomsOccupied();
+  loadGuestNames();
+}
+
+const loadGuestNames = () =>{
+  guestNames = userData.map(user => user.name);
 }
 
 const loadRoomsAvailable = () => {
@@ -250,8 +255,77 @@ const displayError = (array) => {
   }
 }
 
+// autocomplete func below
+const autocomplete = (inp) => {
+  var currentFocus;
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      this.parentNode.appendChild(a);
+      for (i = 0; i < guestNames.length; i++) {
+        if (guestNames[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          b = document.createElement("DIV");
+          b.innerHTML = "<strong>" + guestNames[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += guestNames[i].substr(val.length);
+          b.innerHTML += "<input type='hidden' value='" + guestNames[i] + "'>";
+              b.addEventListener("click", function(e) {
+              inp.value = this.getElementsByTagName("input")[0].value;
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  $(".search-input").change(function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        currentFocus++;
+        addActive(x);
+      } else if (e.keyCode == 38) {
+        currentFocus--;
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        e.preventDefault();
+        if (currentFocus > -1) {
+          if (x) x[currentFocus].click();
+        }
+      }
+  })
+  const addActive = (x) => {
+    if (!x) return false;
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  const removeActive = (x) => {
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  const closeAllLists = (elmnt) => {
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+      x[i].parentNode.removeChild(x[i]);
+    }
+  }
+}
+$(document).click(function (e) {
+    closeAllLists(e.target);
+})
+}
+
+autocomplete(document.querySelector(".search-input"))
 $(".login-btn").click(logIn)
 $(".total-spent-btn").click(toggleTotalSpent)
 $(".check-rates-btn").click(getRates)
+
 
 export default 'index.js'
