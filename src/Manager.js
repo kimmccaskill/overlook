e744526f906a621075a00d83251f198af88bbe1f
Kim McCaskill <kimmccaskill92@gmail.com>
2020-01-14 18:01:57 -0700

    Fix bug where could only delete booking of either manual booking or premade

--- src/Manager.js
+++ src/Manager.js
@@ -9,15 +9,20 @@ class Manager extends User{
 
   deleteBooking(bookings, target) {
     let bookingId = $(target).attr("id")
-    console.log(target)
-    let roomToDelete = bookings.find(booking => booking.id === parseInt(bookingId) || booking.id === bookingId)
+    let roomToDelete;
+    if ($.isNumeric(bookingId)) {
+      roomToDelete = parseInt(bookings.find(booking => booking.id === parseInt(bookingId)).id)
+    } else {
+      roomToDelete = bookings.find(booking => booking.id === bookingId).id
+    }
+
     fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings', {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        id: `${parseInt(roomToDelete.id)}`
+        id: roomToDelete
       })
     })
     fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings', {
